73da50d1d29d5ad7ddfc4f6d5262cc0f7ed7d1eb
Clive <Cliveswe@users.noreply.github.com>
2025-04-04 14:48:00 +0200

    Local cinema menu completed.

--- FlowsLoopsAndStrings.Cinema/LocalCinema.cs
+++ FlowsLoopsAndStrings.Cinema/LocalCinema.cs
@@ -1,23 +1,66 @@
-﻿
-namespace FlowsLoopsAndStrings.Cinema
+﻿using FlowsLoopsAndStrings.Helpers;
+
+namespace FlowsLoopsAndStrings.Cinema;
+
+public class LocalCinema
 {
-    public class LocalCinema
+    public LocalCinema()
     {
-        public LocalCinema()
-        {
 
-        }
-        public void Start()
-        {
-            DisplayMenu();
-        }
-
-        private void DisplayMenu()
-        {
-            Console.WriteLine("********************************");
-            Console.WriteLine("* Welcome to your local Cinema.*");
-            Console.WriteLine("********************************");
-
-        }
     }
-}
+    public void Start()
+    {
+        CinemaClearScreen();
+        CinemaTextColour();
+
+        DisplayMenu();
+
+        ResetTextColour();
+    }
+
+    /// <summary>
+    /// Display a formated local cinema menu.
+    /// </summary>
+    private void DisplayMenu()
+    {
+
+        Console.WriteLine("***********************************************");
+        Console.WriteLine("***      Welcome to your local Cinema.      ***");
+        Console.WriteLine("***********************************************");
+
+        string exit = "";
+
+        foreach (LocalCinemaMenuChoicesEnum menuChoice in
+            Enum.GetValues(typeof(LocalCinemaMenuChoicesEnum)))
+        {
+            string choice = "";
+            if (menuChoice == LocalCinemaMenuChoicesEnum.Exit)
+            {
+                Utilities.GetEnumDecryption(menuChoice, out exit);
+                continue;
+
+            }
+
+            if (Utilities.GetEnumDecryption(menuChoice, out choice))
+            {
+                DisplayChoice((int)menuChoice, choice);
+            }
+        }
+
+        if (!string.IsNullOrEmpty(exit))
+        {
+            DisplayChoice((int)LocalCinemaMenuChoicesEnum.Exit, exit);
+        }
+
+        Console.WriteLine("-----------------------------------------------");
+    }
+
+    private void DisplayChoice(int MenuNumber, string MenuDescription) =>
+        Console.WriteLine($"{MenuNumber} {MenuDescription}");
+
+    private void CinemaTextColour() => Console.ForegroundColor = ConsoleColor.Green;
+    private void ResetTextColour() => Console.ResetColor();
+
+    private void CinemaClearScreen() => Console.Clear();
+
+}//end of class
