3375d483c6c89fb64840758bb749caef94bfa91f
Clive <Cliveswe@users.noreply.github.com>
2025-04-05 17:59:23 +0200

    Added options for children under 5 and Centennial, people of the age of 100.

--- FlowsLoopsAndStrings.Cinema/CinemaPriceCategoriesEnum.cs
+++ FlowsLoopsAndStrings.Cinema/CinemaPriceCategoriesEnum.cs
@@ -4,14 +4,27 @@ namespace FlowsLoopsAndStrings.Cinema;
 
 public enum CinemaPriceCategoriesEnum
 {
+    [Description("No cost")]
+    Free = 0,
+
+    [Description("Child younger than")]
+    Child = 5,
+
     [Description("Youth younger than")]
     Youth = 20,
+
     [Description("Youth ticket price")]
     YouthTicketPrice = 80,
+
     [Description("Pensioner older than")]
     Pensioner = 64,
+
     [Description("Pensioner ticket price")]
     PensionerTicketPrice = 90,
+
+    [Description("Centennial younger than")]
+    Centennial = 100,
+
     [Description("Standard ticket price")]
     StandardTicketPrice = 120
 }
